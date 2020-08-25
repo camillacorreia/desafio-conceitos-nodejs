18feca569b2034a3018b18e238523fcae4721aa6
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-25 20:55:09 -0300

    feat: criação das rotas post e get

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -11,11 +11,17 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
