98c16f29320fd028edde12329615c0ee92957b11
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-25 21:14:00 -0300

    feat: criação da rota put

--- src/app.js
+++ src/app.js
@@ -25,7 +25,25 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repositório não encontrado.'})
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
