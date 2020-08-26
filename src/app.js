af3306956b8d7e1699599eeb51ef740a7063ba05
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-25 21:17:08 -0300

    feat: criação da rota delete

--- src/app.js
+++ src/app.js
@@ -47,7 +47,17 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repositório não encontrado.'})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
